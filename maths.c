fb2533f567d7813c6a352e951c2e1d390d108ffb
ayaelas <assalharras@gmail.com>
2023-11-18 13:06:22 -0800

    mod

--- maths.c
+++ maths.c
@@ -9,6 +9,9 @@
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp;
+	(void)line_number;
+
 	if (*stack == NULL || !(*stack)->next)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
@@ -17,8 +20,9 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n -= (*stack)->n;
+	tmp = *stack;
 	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
+	free(tmp);
 }
 
 /**
