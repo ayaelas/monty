d7d1aca7fdb27c89d36131197030be15f41b1a61
ayaelas <assalharras@gmail.com>
2023-11-19 03:19:56 -0800

    mod

--- maths.c
+++ maths.c
@@ -59,6 +59,8 @@ void mul(stack_t **stack, unsigned int line_number)
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp;
+
 	if (*stack == NULL || ((*stack)->next) == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
@@ -74,8 +76,9 @@ void mod(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n %= (*stack)->n;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
+	tmp = (*stack);
+	(*stack) = (*stack)->next;
+	free(tmp);
 }
 /**
  * duv - function
